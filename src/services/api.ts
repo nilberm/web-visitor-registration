32fb6827b4b103cbd240f38c110b993bcc4ba8ac
nilberm <nilber16@outlookcom>
2024-05-13 12:27:48 -0300

    merge: dev commits

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ const getToken = () => {
 };
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://api-visitor-registration.onrender.com/",
   adapter: axios.defaults.adapter,
 });
 
